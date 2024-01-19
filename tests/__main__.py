87b6ba9e01990f3b129b9d2ea7521d4888a09ebd
GeorgeH87 <GeorgeH87@live.de>
2024-01-19 12:06:40 +0100

    try to run tests

---
+++ tests/__main__.py
@@ -0,0 +1,20 @@
+import os
+import sys
+import unittest
+import logging
+import xmlrunner
+
+
+logging.basicConfig(
+    level = logging.INFO,
+    format='%(created)f:%(levelname)s: %(message)s'
+)
+
+suite = unittest.TestSuite()
+this_dir = os.path.dirname(__file__)
+loader = unittest.TestLoader()
+package_tests = loader.discover(start_dir=this_dir, pattern="*test.py")
+suite.addTests(package_tests)
+runner = xmlrunner.XMLTestRunner(output='test-reports')
+rtn = not runner.run(suite).wasSuccessful()
+sys.exit(rtn)
