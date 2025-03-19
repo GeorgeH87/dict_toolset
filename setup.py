1d7dd0351e9f6b9ebc9ae3c8def80f0a0622ed88
GeorgeH87 <GeorgeH87@live.de>
2025-03-19 23:17:25 +0100

    remove first char

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dict_toolset',
-    version=os.getenv("GITHUB_REF").split("/")[-1],
+    version=os.getenv("GITHUB_REF").split("/")[-1][1:],
     author='George Haddad',
     description='a toolset to work with python dictonaries and arrays',
     long_description_content_type='text/markdown',
